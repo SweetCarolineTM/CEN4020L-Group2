3a3084aff6ee4b088d7af1989aef3fa76e4ad817
Grant <grant.leatherman05@gmail.com>
2025-02-11 23:24:39 -0500

    closes #10 by refactoring the passwordBox to be within the main game loop. it checks if variables have been set after pressing a button to determine whether to display the main menu, the passwordBox, or the currently blank settings page

--- Sketch & Strike/GameFunctions.py
+++ Sketch & Strike/GameFunctions.py
@@ -1,23 +1,5 @@
 import Button
-
-import pygame
-
-def AskPassword(display, passwordBox):
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return None  # Exit on close
-            password = passwordBox.handle_event(event)  
-            if password is not None:  # Return on Enter press
-                return password  
-
-        display.fill((255, 255, 255))  
-        passwordBox.draw(display)  
-        pygame.display.flip()  
-
-def MainMenu(display, buttons):
-    for button in buttons:
-        button.draw(display)
+import pygame 
 
 def PlayerMovement(display, x, y, speed, window_height, window_width, size_of_square):
     keys = pygame.key.get_pressed()
@@ -31,3 +13,5 @@ def PlayerMovement(display, x, y, speed, window_height, window_width, size_of_sq
         x += speed
 
     pygame.draw.rect(display, (0, 128, 255), pygame.Rect(x, y, size_of_square, size_of_square))
+
+    return [x, y, speed]
--- Sketch & Strike/Sketch___Strike.py
+++ Sketch & Strike/Sketch___Strike.py
@@ -1,36 +1,42 @@
-#other files
+# Other game file imports
 from Button import Button
 from PasswordBox import PasswordBox
-from GameFunctions import AskPassword, MainMenu, PlayerMovement
-import Server
-import Client
+from GameFunctions import PlayerMovement
+from Server import hostGame
+from Client import joinGame
 
-#libraries
+# Library imports
 import pygame
 import pymunk
 import sys
 
-#pygame and window initialization
+# Pygame and window initialization
 pygame.init()
 window_width = 800
 window_height = 600
 size_of_square = 50
 fps = 120
 display = pygame.display.set_mode((window_width, window_height))
+# This is necessary for when we implement physics but it was being buggy so I commented it out
 # space = pymunk.space()
 pygame.display.set_caption("Sketch & Strike")
 clock = pygame.time.Clock()
 
-# Main game loop
+# Variable Initialization
 running = True
+hostGamePressed, joinGamePressed, settingsPressed = False, False, False
 x, y = 100, 100
 speed = 5
 
+# Object Initialization
 passwordBox = PasswordBox(250, 250, 300, 50, pygame.Color("gray"), pygame.font)
-hostButton = Button(300, 200, 200, 60, "Host Game", pygame.Color("gray"), pygame.Color("darkgray"), lambda: AskPassword(display, passwordBox))
-joinButton = Button(300, 300, 200, 60, "Join Game", pygame.Color("gray"), pygame.Color("darkgray"), lambda: AskPassword(display, passwordBox))
-buttons = [hostButton, joinButton]
+hostButton = Button(300, 200, 200, 60, "Host Game", pygame.Color("gray"), pygame.Color("darkgray"), lambda: globals().__setitem__('hostGamePressed', True))
+joinButton = Button(300, 300, 200, 60, "Join Game", pygame.Color("gray"), pygame.Color("darkgray"), lambda: globals().__setitem__('joinGamePressed', True))
+settingsButton = Button(300, 400, 200, 60, "Settings", pygame.Color("gray"), pygame.Color("darkgray"), lambda: globals().__setitem__('settingsPressed', True))
+buttons = [hostButton, joinButton, settingsButton]
 
+
+# Main game loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,16 +45,33 @@ while running:
         for button in buttons:
             button.handle_event(event)
 
-    
+        password = passwordBox.handle_event(event)  
+        if password is not None:  # Return on Enter press
+            if (hostGamePressed == True):
+                hostGamePressed = False
+                hostGame(password)
+            elif (joinGamePressed == True):
+                joinGamePressed = False
+                joinGame(password)
 
     display.fill((255, 255, 255))
 
 
-    #change button events so that when clicked it just changes a variable
-    #then if that variable is true, display the password box instead of the main menu
-    MainMenu(display, buttons)
 
-    PlayerMovement(display, x, y, speed, window_height, window_width, size_of_square)
+    # Just left this here for testing and so that it can be moved elsewhere once we start making the game itself
+    x, y, speed = PlayerMovement(display, x, y, speed, window_height, window_width, size_of_square)
+
+
+    # Checks variables for current "screen" and displays accordingly
+    if (hostGamePressed == True or joinGamePressed == True):
+        passwordBox.draw(display)  
+    elif (settingsPressed == True):
+        # Create a function in GameFunctions.py to handle settings
+        pass
+    else:
+        for button in buttons:
+            button.draw(display)
+
 
     # Update the display/space and maintain fps
     pygame.display.flip()
