f87f5bdb121530f6f0849b23b07180a00d8b8e4b
Miles Brosz <milesbrosz1@gmail.com>
2025-02-26 13:00:40 -0500

    network and server architecture
    added network files that control the client, and server that controls the server. Run the server, then in another terminal run the client (Sketch___Strike.py) file. To connect multiple clients, split this client terminal and run the exec again.

--- Sketch & Strike/GameFunctions.py
+++ Sketch & Strike/GameFunctions.py
@@ -1,17 +1,60 @@
+# game functions
+import socket 
+import pygame
 import Button
-import pygame 
+#import pymunk
 
-def PlayerMovement(display, x, y, speed, window_height, window_width, size_of_square):
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and y >= 0:
-        y -= speed
-    if keys[pygame.K_DOWN] and y <= window_height - size_of_square:
-        y += speed
-    if keys[pygame.K_LEFT] and x >= 0:
-        x -= speed
-    if keys[pygame.K_RIGHT] and x <= window_width - size_of_square:
-        x += speed
 
-    pygame.draw.rect(display, (0, 128, 255), pygame.Rect(x, y, size_of_square, size_of_square))
+#player and game functionalities
 
-    return [x, y, speed]
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x,y,width,height)
+        self.vel = 3
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y -= self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y += self.vel
+
+        self.update()
+
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
+    win.fill((255,255,255))
+    player.draw(win)
+    player2.draw(win)
+    pygame.display.update()
+
+def joinGame():
+    foo = False #workon later
