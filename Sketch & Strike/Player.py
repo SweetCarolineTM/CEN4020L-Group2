07d2857d939d2f4408da1f7c81659f1ddc5bfaa7
Caroline Mangrum <cjmang9@gmail.com>
2025-04-21 22:52:40 -0400

    Update Player.py with Physics

--- Sketch & Strike/Player.py
+++ Sketch & Strike/Player.py
@@ -1,4 +1,5 @@
 import pygame
+import pymunk
 
 class Player():
     def __init__(self, x, y, width, height, color):
@@ -10,6 +11,16 @@ class Player():
         self.rect = (x,y,width,height)
         self.vel = 3
 
+        # Pymunk body for physics?
+        mass = 1
+        moment = pymunk.moment_for_box(mass, (width, height))
+        self.body = pymunk.Body(mass, moment) 
+        self.body.position = (x, y)
+
+        self.shape = pymunk.Poly.create_box(self.body, (width, height))
+        self.shape.elasticity = 0.4
+        self.shape.friction = 0.8
+    
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
 
@@ -18,15 +29,19 @@ class Player():
 
         if keys[pygame.K_LEFT]:
             self.x -= self.vel
+            self.body.position = (self.x, self.y)
 
         if keys[pygame.K_RIGHT]:
             self.x += self.vel
+            self.body.position = (self.x, self.y)
 
         if keys[pygame.K_UP]:
             self.y -= self.vel
+            self.body.position = (self.x, self.y)
 
         if keys[pygame.K_DOWN]:
             self.y += self.vel
+            self.body.position = (self.x, self.y)
 
         self.update()
 
